12141cd8c9ceefe24a81efb51308727119b72417
jogindrakumar <joginder9868@outlook.com>
2022-05-27 16:14:30 +0530

    modified:   index.html 	new file:   js/app.js

---
+++ js/app.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+    let emailModal = document.getElementsByClassName('email-modal')[0];
+    let closeModal = document.getElementsByClassName('email-modal__close-btn')[0];
+
+    closeModal.addEventListener('click', () => {
+        emailModal.classList.remove('email-modal--visible');
+    })
+
+    document.addEventListener('mouseleave', () => {
+        emailModal.classList.add('email-modal--visible')
+
+    });
+
+}
